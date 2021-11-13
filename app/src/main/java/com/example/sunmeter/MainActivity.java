aa42a02c86342e10e6b40704e88fecd7c203d964
Yu-Tai Chen <32503459+kevchen1129@users.noreply.github.com>
2021-11-13 17:48:38 -0600

    add circular progress bar , able to count from 1 to 100 with progress bar simultaneously

--- app/src/main/java/com/example/sunmeter/MainActivity.java
+++ app/src/main/java/com/example/sunmeter/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
@@ -19,6 +24,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView progressText;
+    int i = 0;
 
 
 
@@ -27,6 +35,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progress_bar);
+        progressText = findViewById(R.id.progres_text);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(i <= 100){
+                    //android.os.SystemClock.sleep(5000);
+                    progressText.setText(""+i);
+                    progressBar.setProgress(i);
+                    i++;
+                    handler.postDelayed(this,200);
+
+
+                }else{
+                    handler.removeCallbacks(this);
+
+                }
+
+            }
+        },200);
+
        Button button = findViewById(R.id.recycle_btn);
        button.setOnClickListener(new View.OnClickListener() {
            @Override
